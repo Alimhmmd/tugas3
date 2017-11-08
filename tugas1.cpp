c00ff4378fa51bb96de1a5c38559c7d08b9be743
Ali <alimhmmd434@gmail.com>
2017-11-08 20:59:14 +0700

    tugas ali.cpp

--- tugas1.cpp
+++ tugas1.cpp
@@ -3,4 +3,16 @@
 using namespace std;
 
 int main () {
+	int x, y, z;
+	cout << " Input nilai x = ";
+	cin >> x;
+	
+	cout << "input nilai y = ";
+	cin >> "y;
+	
+	if (x > y) {
+		z = y - x;
+		cout<<abs (z)<<endl;
+		cout<<"hasil positif";
+	}
 }
