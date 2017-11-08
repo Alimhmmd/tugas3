9d553e86ac03bf660ef2cd6d23137b8884672228
Ali <alimhmmd434@gmail.com>
2017-11-08 21:10:17 +0700

    tugas ali.cpp

--- tugas5.cpp
+++ tugas5.cpp
@@ -7,4 +7,10 @@ int main () {
 	
 	cout << "Masukkan tinggi badan dalam cm : ";
 	cin >> tinggi;	
+	
+	cout << "Masukkan berat badan dalam kg : ";
+	cin >> berat;
+	
+	if (berat<tinggi/2.5) {
+		cout << "Hasil : Kurus " <<endl;
 }
